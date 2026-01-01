f0df592b620bff09076742e466fffa6005fcd1aa
Scott McGrath <scrawlon@gmail.com>
2026-01-01 17:12:23 -0500

    Code cleanup.

--- src/js/noc-webmidi.js
+++ src/js/noc-webmidi.js
@@ -1,9 +1,6 @@
 (function() {
   "use strict";
-  var synth = require('./novation-circuit-synth.js'),
-    drum = require('./novation-circuit-drums.js'),
-    session = require('./novation-circuit-session.js'),
-    circuitMidiApp = require('./index.js');
+  const circuitMidiApp = require('./index.js');
 
   window.circuitMidiApp = circuitMidiApp;
 })();
